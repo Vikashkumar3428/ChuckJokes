5a6d27cbed0f4cb3207e2466d0bebcd6f2c93597
Vikashkumar3428 <vkumar3428@gmail.com>
2021-04-11 20:33:01 +0530

    committed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
+class App extends Component {
+  constructor(){
+    super();
+    this.state={
+      data:false
+    }
+  }
+  componentDidMount()
+  {
+    let url = "https://api.chucknorris.io/jokes/categories";
+    fetch(url, {
+      method:'GET',
+      headers:{
+        'Accept':'application/json',
+        'Content-Type':'application/json',
+      }
+    }).then(result=>{
+      result.json().then((res) =>{
+        this.setState({data:res})
+      })
 
-function App() {
+    })
+  }
+
+  render(){
+    const data = this.state.data;
+    console.warn(data);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        {
+          data?
+          <div className="fluid-contanier bg-aqua">
+            {
+              data.map(function(item, i){
+                return <Button variant= "light" key={i}>{data}</Button>
+              })
+            }
+          {/* <button type="button" class="btn btn-light">{data}</button> */}
+        </div>
+        : <p>Please Wait......</p>
+        }
+      </div>
     </div>
   );
+}
 }
 
 export default App;
