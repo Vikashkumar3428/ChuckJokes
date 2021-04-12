5e63b3bc290f05029c113faa7454f2908b6a4932
Vikashkumar3428 <vkumar3428@gmail.com>
2021-04-12 08:48:58 +0530

    Completed

--- src/App.js
+++ src/App.js
@@ -1,44 +1,64 @@
-import React, { useState} from 'react';
+
 import './App.css';
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useEffect, useState} from 'react';
+import ReactDOM from 'react-dom';
 function App() {
   
-  const [cate,setCate] = useState('None');
+  const [cat,setCat] = useState('None');
 
-  function cat(item) {
-    setCate(item);
+
+  const [data, setData] = useState([]);  
+  const [jokes, setJokes] = useState('');   
+   
+  function getJokes(category){
+    if(cat!==category){
+    ReactDOM.findDOMNode(document.getElementById(cat)).classList.remove("active")
+    setCat(category);
+    ReactDOM.findDOMNode(document.getElementById(category)).classList.add("active")
+    }
+    fetch("https://api.chucknorris.io/jokes/random?categories="+category)
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setJokes(result.value);
+        }
+      )
   }
 
-  const [data, setData] = useState([]);   
-    
+  useEffect(() => {
+    fetch("https://api.chucknorris.io/jokes/categories")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          getJokes(result[0])
+          setData(result);
+        }
+      )
+  }, [])
   
-  fetch('https://api.chucknorris.io/jokes/categories')
-  .then(response => response.json())
-  .then((json) =>{
-    console.log(json);
-    setData(json);
-    });
-   
-  const items = data;
   return (
     <div className=""><h1 className="mt-5"><b>Chuck Norries</b></h1>
     <div className="App">
       
      <div className="pb-4 ">
         <br />
-        {items.map(item=>
-        <Button className="button my-2 mx-3" onClick={cat(item)} variant="light" size ="sm" >{item}</Button>
-        )}
+        {data ? data.map((item,id)=>
+        <Button key={id} id={item} className="button my-2 mx-3" onClick={()=>{getJokes(item);}} variant="light" size ="sm" >{item}</Button>
+        ):<p>Loading...</p>}
         
         </div> 
      </div>
      <div className="">
-          <h3 className="cat_selected">Selected Category : {cate}</h3>
-            <div className="para_div"><p className="para py-10">When In A Bar, You Can Order A Drink Called A "Chuck Norris".It is Also Known As A "Bloody Mary", If Your Name Happens To Be Mary.</p></div>
+          <h3 className="cat_selected">Selected Category : {cat}</h3>
+          {
+            jokes?
+              <div className="para_div"><p className="para py-10">{jokes}</p></div>:"Loading..."
+          }
         </div>
         
-        <a href="#"><p className="joke">New Joke</p></a>
+        <Button id={cat} className="joke" onClick={()=>{getJokes(cat);}} variant="light" size ="sm" >New Joke</Button>
         
      </div>
   );
